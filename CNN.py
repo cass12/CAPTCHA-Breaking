367b59f9b231e4e42bfd3009fcab223fcd4c25a9
cass12 <64856925+cass12@users.noreply.github.com>
2020-05-14 20:21:43 +0300

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import os
+import imutils
+from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+
+def read_img(image):
+    return cv2.imread(character_image)
+
+def display_img(image):
+    cv2.imshow('Characters', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+
+def normalize_img(image, new_width=20, new_height=20):
+    #define image dims
+    #slicing, start:stop-1
+    (h, w) = image.shape[:2]
+    if w > h:
+        image = imutils.resize(image, width = new_width)
+    else:
+        image = imutils.resize(image, height = new_height)
+    #define size of padding area
+    h_padding = int((new_height - image.shape[0])/2)
+    w_padding = int((new_width - image.shape[1])/2)
+    white = [255,255,255]
+    img_with_border = cv2.copyMakeBorder(image, h_padding, h_padding, w_padding, w_padding, cv2.BORDER_CONSTANT, value = white)
+    img_border_resized = cv2.resize (image, (new_width, new_height), interpolation = cv2.INTER_AREA)
+    return img_with_border
+
+#keras requires 4 dims
+def resize_to_keras(image):
+    return np.expand_dims(image, axis=2)
+
+imgs = []
+labels = []
+extracted_characters_folder = "/home/cas/Desktop/extracted_characters"
+for character_image in paths.list_images(extracted_characters_folder):
+    print(character_image)
+    single_character = read_img(extracted_characters_folder)
+    #display_img(single_character)
+    grayscaled = grayscale_img(single_character)
+    normalized = normalize_img(grayscaled)
+    #display_img(normalized)
+    keras_img = resize_to_keras(normalized)
+    imgs.append(keras_img)
+    label = character_image.split(os.path.sep)[-2]
+    labels.append(label)
+
+
+bw_to_binary = np.array(imgs, dtype=object)/255.0 #binarizing the colours
+labels = np.array(labels)
+
+lb = LabelBinarizer().fit(labels) #training on the labels
+Y = lb.transform(labels) #encodes each label into a vector for its specific class 
