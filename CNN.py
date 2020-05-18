c9a012e28f007d5daf37077b6037715b3d5644fb
cass12 <64856925+cass12@users.noreply.github.com>
2020-05-18 18:51:12 +0300

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -64,10 +64,10 @@ binary_class_encoder = lb.transform(labels) #encodes each label into a vector fo
 #A simple CNN Sequential architecture with one in/out tensor
 nr_classes = len(set(labels))
 my_model = Sequential 
-model.add(Conv2D(20, (3,3), padding = "same", input(20,20,1), activation = "relu" )) 
-model.add(MAXPooling2D(pool_size = (2,2), strides = (2,2)))
-model.add(Conv2D(50, (3,3), padding = "same", input(20,20,1), activation = "relu")) 
-model.add(MAXPooling2D(pool_size = (2,2), strides = (2,2))) 
-model.add(Flatten()) 
-model.add(Dense(384, activation = "relu")) 
-model.add(Dense(nr_classes, activation = "softmax"))
+my_model.add(Conv2D(20, (3,3), padding = "same", input(20,20,1), activation = "relu" )) 
+my_model.add(MAXPooling2D(pool_size = (2,2), strides = (2,2)))
+my_model.add(Conv2D(50, (3,3), padding = "same", input(20,20,1), activation = "relu")) 
+my_model.add(MAXPooling2D(pool_size = (2,2), strides = (2,2))) 
+my_model.add(Flatten()) 
+my_model.add(Dense(384, activation = "relu")) 
+my_model.add(Dense(nr_classes, activation = "softmax"))
